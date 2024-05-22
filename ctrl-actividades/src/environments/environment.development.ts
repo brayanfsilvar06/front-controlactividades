519c140fe83d32106ef344b1f787b9a7d682bd65
BrayanFSilvaR <brayanf.silvar@gmail.com>
2024-05-21 20:30:50 -0500

    Configuracion dllo

--- ctrl-actividades/src/environments/environment.development.ts
+++ ctrl-actividades/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    URL_SERVICE: 'http://localhost:8080/ControlActividades/APIRest/'
+    /*URL_SERVICE: 'http://localhost:8080/ControlActividades/APIRest/'*/
+    URL_SERVICE: 'http://apps-devbfsr.us-east-2.elasticbeanstalk.com/APIRest/'
 };
